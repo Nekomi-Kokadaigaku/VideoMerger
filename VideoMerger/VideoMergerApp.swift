e8cbe7f9eb73cf9fbe30b25a474df6c23be17b3a
1ris_W <ediath462@gmail.com>
2025-02-26 23:35:03 +0800

    Initial Commit

---
+++ VideoMerger/VideoMergerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoMergerApp.swift
+//  VideoMerger
+//
+//  Created by Iris on 2025-02-26.
+//
+
+import SwiftUI
+
+@main
+struct VideoMergerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
